306eb309102964d040771731041d3f14303dc963
Yury Butskevich <bxyura@gmail.com>
2023-12-18 13:24:30 +0300

    feat: complete dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (Array.isArray(members) === true) {
+    let res = '';
+    members.forEach(name => {
+      if (typeof name === 'string') {
+        res += name.trim()[0];
+      }
+    });
+    return res.toUpperCase().split('').sort().join('');
+  } else return false;
 }
 
 module.exports = {
