1e9b2a55247bb75a351e08b074dc478d0fb3f874
Yury Butskevich <bxyura@gmail.com>
2023-12-18 15:11:43 +0300

    feat: complete extended-reapeter

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  const repeatTimes = options.repeatTimes !== undefined ? options.repeatTimes : 1;
+  const separator = options.separator !== undefined ? options.separator : '+';
+  const addition = options.addition !== undefined ? options.addition : '';
+  const additionRepeatTimes = options.additionRepeatTimes !== undefined ? options.additionRepeatTimes : 1;
+  const additionSeparator = options.additionSeparator !== undefined ? options.additionSeparator : '|';
+
+  let resStr = '';
+  let resAdd = '';
+
+  for (let i = 1; i <= additionRepeatTimes; i += 1) resAdd += addition + ((i < additionRepeatTimes) ? additionSeparator : '');
+  for (let i = 1; i <= repeatTimes; i += 1) resStr += str + resAdd + ((i < repeatTimes) ? separator : '');
+
+  return resStr;
 }
 
 module.exports = {
