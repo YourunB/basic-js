f596ad64c001cb1f56963c56b47635edf2abd689
Yury Butskevich <bxyura@gmail.com>
2023-12-18 12:52:12 +0300

    feat: complete dns-stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let obj = {};
+  domains.forEach(domain => {
+    let domainsParts = domain.split('.').reverse();
+    console.log(domainsParts)
+    let resDns = '';
+    domainsParts.forEach(part => {
+      resDns += '.' + part;
+      if (!obj[resDns]) obj[resDns] = 1;
+      else obj[resDns] = obj[resDns] + 1;
+    })
+  });
+  return obj;
 }
 
 module.exports = {
